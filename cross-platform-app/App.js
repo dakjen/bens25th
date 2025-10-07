937df2377b105ac63f2ccc4a0c632f1c0d08eb31
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-07 15:57:15 -0400

    fix: Correct typo in ImagePicker import statement in App.js

--- cross-platform-app/App.js
+++ cross-platform-app/App.js
@@ -6,7 +6,7 @@ import { useFonts } from 'expo-font';
 import { Manrope_400Regular, Manrope_500Medium, Manrope_700Bold } from '@expo-google-fonts/manrope';
 import { PermanentMarker_400Regular } from '@expo-google-fonts/permanent-marker';
 import * as SplashScreen from 'expo-splash-screen';
-import *s ImagePicker from 'expo-image-picker'; // Added ImagePicker import
+import * as ImagePicker from 'expo-image-picker'; // Added ImagePicker import
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
